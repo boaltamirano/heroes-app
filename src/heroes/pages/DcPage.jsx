f5e29e92c4a4abc6de7e7d13bfbe08512b9bca1c
boaltamirano <brayanaltamiranoperez@gmail.com>
2023-04-21 23:57:09 -0500

    feat: heroes card

--- src/heroes/pages/DcPage.jsx
+++ src/heroes/pages/DcPage.jsx
@@ -1,6 +1,11 @@
+import { HeroList } from "../components"
 
 export const DcPage = () => {
     return (
-        <h1>Dc Page</h1>
+        <>
+            <h1>Dc Page</h1>
+            <hr />
+            <HeroList publisher={'DC Comics'}/>
+        </>
     )
 }
