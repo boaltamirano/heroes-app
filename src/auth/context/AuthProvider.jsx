65208aa4c02b9b71aa624c18035de4695f7390ef
boaltamirano <brayanaltamiranoperez@gmail.com>
2023-04-22 16:46:07 -0500

    feat: context for login

---
+++ src/auth/context/AuthProvider.jsx
@@ -0,0 +1,38 @@
+import { useReducer } from 'react'
+import { authReducer } from './authReducer'
+import { AuthContext } from './AuthContext'
+import { types } from '../types/types'
+
+const init = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    return {
+        logged: !!user,
+        user: user,
+    }
+}
+
+export const AuthProvider = ({children}) => {
+
+    const [ authState, dispatch] = useReducer(authReducer, {}, init);
+
+    
+    const login = ( name = '' ) => {
+        const user = {id: 'ABC', name: name};
+        const action = {
+            type: types.login,
+            payload: user
+        }
+        localStorage.setItem('user', JSON.stringify(user));
+        dispatch(action)
+    }
+
+    return (
+        <AuthContext.Provider value={{
+            ...authState,
+            login: login
+        }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
